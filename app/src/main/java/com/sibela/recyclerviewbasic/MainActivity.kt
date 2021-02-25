09336f0c2a4dd010e40d50494d5e7cc8aad7aca8
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-25 10:31:12 -0300

    Connecting recyclerview and adapter

--- app/src/main/java/com/sibela/recyclerviewbasic/MainActivity.kt
+++ app/src/main/java/com/sibela/recyclerviewbasic/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.sibela.recyclerviewbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: CharactersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CharactersAdapter()
+        charactersRecycler.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.adapter
+        }
+
+        val characters = getCharacters()
+        adapter.setCharacters(characters)
+    }
+
+    fun getCharacters(): List<Character> {
+        return arrayListOf(
+            Character("1", "Spider Man"),
+            Character("2", "Iron Man"),
+            Character("3", "Hulk"),
+            Character("4", "Captain America"),
+            Character("5", "Thanos"),
+            Character("6", "Wolverine"),
+            Character("7", "Deadpool"),
+            Character("8", "Dr. Strange"),
+            Character("9", "Black Widow"),
+            Character("10", "Thor"),
+            Character("11", "Vision"),
+            Character("12", "Nick Fury"),
+            Character("13", "Venom"),
+            Character("14", "Galactus")
+        )
     }
 }
